445396c83dd91ee750f9e29e24acc93d250d69df
doha <doha.sassa@uit.ac.ma>
2023-03-15 20:51:14 +0100

    headerStats : add cards

--- src/app/components/headers/header-stats/header-stats.component.ts
+++ src/app/components/headers/header-stats/header-stats.component.ts
@@ -5,7 +5,33 @@ import { Component, OnInit } from "@angular/core";
   templateUrl: "./header-stats.component.html",
 })
 export class HeaderStatsComponent implements OnInit {
+
+  CardObjects: any[]=[
+    {
+      title :"ChitChat",
+      description : "Remembers what user said earlier in the conversation",
+      icon : "fas fa-comment-dots"
+    },
+    {
+      title :"ChitChat",
+      description : "Allows user to provide follow-up corrections",
+      icon : "fas fa-comment-dots"
+    },
+    {
+      title :"ChitChat",
+      description : "Trained to decline inappropriate requests",
+      icon : "fas fa-comment-dots"
+    },
+    {
+      title :"ChitChat",
+      description : "Limited knowledge of world and events after 2021",
+      icon : "fas fa-exclamation-triangle"
+    }
+  ]
   constructor() {}
 
   ngOnInit(): void {}
+
+
+  
 }
