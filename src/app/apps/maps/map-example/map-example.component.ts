3557eb76e581c3e468ebf7264a67d7dd4eaccb0f
doha <doha.sassa@uit.ac.ma>
2023-03-15 20:50:48 +0100

    MapExamle : add loading and spiner

--- src/app/apps/maps/map-example/map-example.component.ts
+++ src/app/apps/maps/map-example/map-example.component.ts
@@ -10,13 +10,15 @@ declare const google: any;
 export class MapExampleComponent implements OnInit {
 
   searchResults : any;
+  loading : boolean = true;
   constructor(private _chatBarService : ChatBarService) {}
 
   ngOnInit(): void {
-
+    this.loading = true
     this._chatBarService.emitSubject.subscribe((res : any)=>{
       if(res != null){ 
          this.searchResults = res.choices[0].text;
+         this.loading = false;
          console.log(this.searchResults);
       }
     });
